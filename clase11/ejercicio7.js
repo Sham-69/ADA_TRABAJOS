5fab085dfaef5daceacef46a553aa6007bf5ae9f
Sham-69 <yamilatapia93@outlook.com>
2025-08-30 22:21:52 -0300

    Agregando ejercicio

--- clase11/ejercicio7.js
+++ clase11/ejercicio7.js
@@ -5,3 +5,37 @@
 // ingresada, el programa deberá calcular y mostrar el resultado. Si el usuario 
 // ingresa una operación inválida, el programa debe mostrar un mensaje de error. 
 // Usa switch para resolverlo.
+
+const prompt = require ('prompt-sync')();
+
+let numero1 = parseInt(prompt('Ingrese un numero: '));
+let numero2 = parseInt(prompt('Ingrese otro numero: '));
+let operacion = prompt('Ingrese si desea sumar, restar, multiplicar o dividir: ').toLocaleLowerCase();
+
+switch (operacion) {
+    case "sumar":
+        console.log(numero1 + numero2);
+        
+        break;
+
+    case "restar":
+        console.log(numero1 - numero2);
+        
+        break;
+
+    case "multiplicar":
+        console.log(numero1 * numero2);
+        
+        break;
+
+    case "dividir":
+        console.log(numero1 / numero2);
+        
+        break;
+    
+        
+    default:
+        console.log('Error. Por favor ingrese la operacion correctamente');
+        
+        break;
+}
