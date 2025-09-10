899b3c0f30a53b44ec79dcbe88b68ed3ec316e71
Sham-69 <yamilatapia93@outlook.com>
2025-09-10 20:23:44 -0300

    Converti horas a minutos

---
+++ Clase13/ejercicio6.js
@@ -0,0 +1,13 @@
+// Ejercicio 6: Convertir a minutos 
+// Crea una función declarada llamada convertirHorasAMinutos que reciba un 
+// número de horas como parámetro y devuelva el total en minutos.
+
+const prompt = require('prompt-sync')();
+let horas = parseInt(prompt('Ingrese cantidad de horas: '));
+
+function convertirHorasAMinutos(horas){
+    let minutos = horas * 60;
+    return minutos;
+}
+
+console.log(`El total de minutos para ${horas} horas es`, convertirHorasAMinutos(horas));
