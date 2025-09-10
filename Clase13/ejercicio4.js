e08632ada4f3e1379739d46db7e5612483f28ae1
Sham-69 <yamilatapia93@outlook.com>
2025-09-10 20:11:36 -0300

    Funcion declarada

---
+++ Clase13/ejercicio4.js
@@ -0,0 +1,23 @@
+// Ejercicio 4: Número par o impar 
+// Crea una función declarada llamada esPar que reciba un número y devuelva 
+// "Es par" si el número es par o "Es impar" si el número es impar. 
+// Usa una variable local para guardar el resultado.
+
+
+const prompt = require ('prompt-sync')();
+
+let numero = parseInt(prompt('Ingrese un numero: '));
+
+function esPar (numero) {
+    let resultado; 
+    if (numero % 2 === 0) {
+        resultado = "Es par";
+        
+    } else {
+        resultado ="Es impar"
+    }
+    return resultado;
+}
+
+console.log(esPar(numero));
+
